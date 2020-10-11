d90c77a26a21ee52eb2a78f90d4b671a27224ca5
KarlaReyesA01285392 <70342598+KarlaReyesA01285392@users.noreply.github.com>
2020-10-11 13:30:42 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+for (let index = 0; index < 4; index++) {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.West)
+}
+basic.forever(function () {
+	
+})
